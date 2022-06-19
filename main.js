22725498e04ab1210e6a4c4770690d9193596a37
OhmPatil <workpatil10@gmail.com>
2022-06-19 13:31:43 +0530

    Add pw validation and change css layout units

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const password = document.querySelector('input[name=password]')
 const confirm_password = document.querySelector('input[name=confirm-password]')
 const pw_error = document.getElementById('pw-error')
 
+password.addEventListener('focusin', function(){
+    password.classList.remove('error')
+    confirm_password.classList.remove('error')
+    pw_error.textContent = ''
+})
+
 confirm_password.addEventListener('keyup', function(){
     if (password.value != confirm_password.value){
         password.classList.add('error')
@@ -12,6 +18,15 @@ confirm_password.addEventListener('keyup', function(){
     if (password.value == confirm_password.value){
         password.classList.remove('error')
         confirm_password.classList.remove('error')
-        pw_error.textContent = ""
+        pw_error.textContent = ''
     }
 })
+
+document.querySelector('form').onsubmit = function(){
+    console.log('works');
+    return false
+}
+
+
+// Adding credit line 
+document.getElementById('mycredit').textContent = 'Ohm Patil © 2022'
